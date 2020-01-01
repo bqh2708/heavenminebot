65e54ed92c9c335abb4d4d179671d7aebb800421
bqh2708 <59406663+bqh2708@users.noreply.github.com>
2020-01-01 13:38:52 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+const { Client } = require("discord.js");
+const { Guild } = require("discord.js");
+const { config } = require("dotenv");
+
+// Declares our bot,
+// the disableEveryone prevents the client to ping @everyone
+const client = new Client({
+    disableEveryone: true
+});
+const guild = new Guild({
+    disableEveryone: true
+});
+
+config({
+    path: __dirname + "/.env"
+})
+
+// When the bot's online, what's in these brackets will be executed
+client.on("ready", () => {
+    console.log(`Hi, ${client.user.username} is now online!`);
+
+    // Set the user presence
+    client.user.setPresence({
+        status: "online",
+        game: {
+            name: "NewHeaven",
+            type: "Playing"
+        }
+    });
+})
+
+// When a message comes in, what's in these brackets will be executed
+client.on("message", async message => {
+    const prefix = "hm!";
+
+    // If the author's a bot, return
+    // If the message was not sent in a server, return
+    // If the message doesn't start with the prefix, return
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    // Arguments and command variable
+    // cmd is the first word in the message, aka the command
+    // args is an array of words after the command
+    // !say hello I am a bot
+    // cmd == say (because the prefix is sliced off)
+    // args == ["hello", "I", "am", "a", "bot"]
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = (args.shift()).toLowerCase();
+    if (cmd === "say") {
+        // Check if you can delete the message
+        if (message.deletable) message.delete();
+
+        if (args.length === 0) {
+            message.reply(`Nothing to say?`).then(m => m.delete(5000));
+            return
+        };
+
+        // Role color
+        const roleColor = message.guild.me.highestRole.hexColor;
+
+        // If the first argument is embed, send an embed,
+        // otherwise, send a normal message
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new RichEmbed()
+                .setDescription(args.slice(1).join(" "))
+                .setColor(roleColor === "#000000" ? "#ffffff" : roleColorv)
+                .setTimestamp()
+                .setImage(client.user.displayAvatarURL)
+                .setAuthor(message.author.username, message.author.displayAvatarURL);
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(args.join(" "));
+        }
+    }
+});
+
+client.on('guildMemberUpdate', (oldData, newData) => {
+    if (!oldData._roles.includes('660671267917135883') && newData._roles.includes('660671267917135883')) {
+        oldData.guild.members.get(oldData.user.id).setNickname(`HM | ${newData.user.username}`)
+        // oldData.guild.members.get(oldData.user.id).setRoles("661800721251041291")
+    } else if (!newData._roles.includes('660671267917135883')) {
+        oldData.guild.members.get(oldData.user.id).setNickname('');
+
+    }
+})
+
+
+// Login the bot
+client.login(process.env.TOKEN);
