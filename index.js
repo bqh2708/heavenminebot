2f2f64a87b00343b9f465366a4ffa2b3f75638ac
bqh2708 <59406663+bqh2708@users.noreply.github.com>
2020-01-12 19:07:07 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -354,30 +354,7 @@ client.on("message", async message => {
             let results = await search(query.first().content, opts).catch(err => console.log(err));
             if (results) {
                 let youtubeResults = results.results;
-                let i = 0;
-                let titles = youtubeResults.map(result => {
-                    i++;
-                    return i + ") " + result.title;
-                });
-                console.log(titles);
-                message.channel.send({
-                    embedSearch: {
-                        title: 'Select which song you want by typing the number',
-                        description: titles.join("\n")
-                    }
-                }).catch(err => console.log(err));
-
-                filter = m => (m.author.id === message.author.id) && m.content >= 1 && m.content <= youtubeResults.length;
-                let collected = await message.channel.awaitMessages(filter, { maxMatches: 1 });
-                let selected = youtubeResults[collected.first().content - 1];
-
-                embedSearch = new RichEmbed()
-                    .setTitle(`${selected.title}`)
-                    .setURL(`${selected.link}`)
-                    .setDescription(`${selected.description}`)
-                    .setThumbnail(`${selected.thumbnails.default.url}`);
-
-                message.channel.send(embedSearch);
+                console.info(youtubeResults);
             }
             break;
 
