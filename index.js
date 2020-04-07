eb7f238e6e52d4e124ba6a38b50acf49ea4b63ff
bqh2708 <59406663+bqh2708@users.noreply.github.com>
2020-04-08 00:11:43 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -127,28 +127,6 @@ client.on("message", async message => {
                         });
                         break;
 
-                    case 'removeExp':
-
-                        if (uid === "376557542177767445") {
-                            let sqlUpdate;
-                            let sqlSelect = `SELECT * FROM TBL_EXP WHERE USER_ID = ${args[1]}`;
-
-                            pool.query(sqlSelect, (err, result) => {
-                                if (err) {
-                                    console.info(err);
-                                    return;
-                                };
-                                sqlUpdate = `UPDATE TBL_EXP SET EXP ${result.rows[0].exp - args[2]}`
-                            });
-
-                            pool.query(sqlUpdate, (err, result) => {
-                            });
-
-                        } else {
-                            message.reply('Xin lỗi. Bạn không có quyền !').then(m => m.delete(10000));
-                        }
-                        break;
-
                     case 'help': case '-h':
                         replyLevelHelpMessage(message);
                         break;
@@ -312,7 +290,6 @@ client.on("message", async message => {
                     // XP display
                     ctx.font = "16px Consolas";
                     ctx.fillStyle = "black";
-                    ctx.textAlign = 'center'
                     ctx.fillText(`XP: ${currentXp} / ${nextXp}`, 580, 43);
 
                     // Hiển thị LEVEL
@@ -358,20 +335,6 @@ client.on("message", async message => {
         case '-m':
             if (args[0]) {
                 switch (args[0]) {
-                    case 'list': case '-lt':
-                        var content = "";
-                        musicQueue.some((music, index) => {
-                            content += `
-                            ${index + 1}. ${music.title}`
-                        });
-
-                        embed = new RichEmbed()
-                            .setColor("#cc66ff")
-                            .setAuthor('Danh sách bài hát đang chờ', client.user.displayAvatarURL)
-                            .setDescription(content);
-                        message.channel.send(embed);
-                        break;
-
                     case 'play': case '-p':
                         if (args[1]) {
                             if (!curentChannel) {
