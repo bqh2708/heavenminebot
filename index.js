05e9512747255bd4c500d1b7ac1e8aed8cd29f0c
bqh2708 <59406663+bqh2708@users.noreply.github.com>
2020-06-16 14:35:51 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { config } = require("dotenv");
 var fs = require('fs');
 const Canvas = require('canvas');
 
-
+const translate = require('@vitalets/google-translate-api');
 
 config({
     path: __dirname + "/.env"
@@ -13,6 +13,10 @@ config({
 // Declares our bot,
 // the disableEveryone prevents the client to ping @everyone
 const client = new Client({
+    disableEveryone: true
+});
+
+const guild = new Guild({
     disableEveryone: true
 });
 
@@ -30,6 +34,7 @@ client.on("ready", () => {
     });
 
 })
+
 
 // Login the bot
 client.login(process.env.TOKEN);
@@ -55,6 +60,52 @@ client.on("message", async message => {
     // !say hello I am a bot
     // cmd == say (because the prefix is sliced off)
     // args == ["hello", "I", "am", "a", "bot"]
+
+    var roles = message.member._roles;
+    var isbOT = roles.indexOf('693852491917295616') === -1 ? false : true;
+    if (isbOT) {
+        return;
+    }
+
+    if (message.content.toLowerCase().startsWith('-rolemenu')) {
+        return;
+    }
+
+    if (!message.content.toLowerCase().startsWith(prefix)) {
+        var currentLanguage;
+        var maybeEnValue, maybeViValue;
+        translate(message.content, { to: 'en' }).then(res => {
+            currentLanguage = res.from.language.iso;
+
+            maybeEnValue = currentLanguage !== 'en' ? `English : ${res.text}
+            
+            ` : '';
+
+            translate(message.content, { to: 'vi' }).then(res => {
+                maybeViValue = currentLanguage !== 'vi' ? `Vietnamese : ${res.text}
+                
+                ` : '';
+
+                var content = `${message.content}
+                --------------------------------------------------
+                ${maybeEnValue}${maybeViValue}`;
+
+                embed = new RichEmbed()
+                    .setColor("#cc66ff")
+                    .setAuthor(`ZV Translate`, client.user.displayAvatarURL)
+                    .setDescription(content);
+                message.channel.send(embed);
+
+            }).catch(err => {
+                viValue = 'error';
+            });
+
+        }).catch(err => {
+            enValue = 'error'
+        });
+        return;
+    };
+
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = (args.shift()).toLowerCase();
 
@@ -81,9 +132,51 @@ client.on("message", async message => {
             message.channel.send(attachment);
             break;
 
+        case 'aoo':
+            // switch (args[0].toUpperCase()) {
+            //     case '17ZV':
+            //         getMemberAOO(message, '17ZV', '722342492522938369');
+            //         break;
+
+            //     case '17FV':
+            //         getMemberAOO(message, '17FV', '722248375986028658');
+            //         break;
+
+            //     case 'QZ7':
+            //         getMemberAOO(message, 'QZ_7', '722248654823489597');
+            //         break;
+
+            //     case 'QZ8':
+            //         getMemberAOO(message, 'QZ_8', '722255625572057179');
+            //         break;
+            // }
+            break;
+
         default:
             break;
 
 
     }
 });
+
+
+function getMemberAOO(message, alliName, id) {
+    var nickname;
+    var arrMember = client.guilds.get(
+        '693830119676051477').members.filter(member => member._roles.indexOf(id) > -1);
+    var content = '';
+    for (const item of arrMember) {
+        nickname = arrMember.get(item[0]).nickname;
+        if (nickname === null) {
+            nickname = arrMember.get(item[0]).user.username;
+            content += `
+            ${nickname}`
+        }
+    }
+
+    embed = new RichEmbed()
+        .setColor("#cc66ff")
+        .setAuthor(` List of participants in AOO in ${alliName}`, client.user.displayAvatarURL)
+        .setDescription(content);
+    message.channel.send(embed);
+};
